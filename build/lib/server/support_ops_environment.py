505aadc45aa474bc78bc9ed89fd1caa1a0239640
Ankan Ghosh <ghoshankan005@gmail.com>
2026-03-27 19:55:08 +0530

    Clean repo

--- build/lib/server/support_ops_environment.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility wrapper for the new environment module."""
-
-from server.environment import IncidentCommanderEnvironment
-
-SupportOpsEnvironment = IncidentCommanderEnvironment
-
-__all__ = ["IncidentCommanderEnvironment", "SupportOpsEnvironment"]
